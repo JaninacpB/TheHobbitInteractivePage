e9ee677bd34936c94a40987f160d2966a3330280
JaninacpB <61713354+JaninacpB@users.noreply.github.com>
2022-11-16 17:28:38 +0100

    rätsel 1 und Vokabel 1 hintereinander
    Lied gefixed und kein Button input mehr

--- TheHobbitInteractivePageWorkingCopy/TheHobbit/TheHobbit.ino
+++ TheHobbitInteractivePageWorkingCopy/TheHobbit/TheHobbit.ino
@@ -10,18 +10,30 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2);  //Speicherort meines
 
 const int switchPinKipp = 6;  // Kippschalter
-ezButton button(7);       // create ezButton object that attach to pin 7;
+ezButton button(7);           // create ezButton object that attach to pin 7;
 
 //Kippschalter
 int switchStateKipp = 0;
 int prevSwitchStateKipp = 0;
 
 int melody[] = {
-  0, 0, NOTE_B3, NOTE_D4, NOTE_E4, 0, NOTE_G4, NOTE_A4, NOTE_B4, NOTE_A4, NOTE_G4, NOTE_FS4, NOTE_E4, 0, NOTE_B3, NOTE_E4, NOTE_FS4, NOTE_FS4, NOTE_FS4, 0, NOTE_G4, NOTE_A4, NOTE_G4, NOTE_FS4, NOTE_E4, 0, NOTE_B3, NOTE_B3, NOTE_D4, NOTE_E4, NOTE_E4, NOTE_E4, NOTE_G4, NOTE_A4, NOTE_G4, NOTE_FS4, NOTE_D4, NOTE_E4, 0, NOTE_B3, NOTE_E4, NOTE_FS4, NOTE_FS4, NOTE_FS4, 0, NOTE_G4, NOTE_A4, NOTE_G4, NOTE_FS4, 0, NOTE_FS4, 0, NOTE_G4, NOTE_A4, NOTE_FS4, NOTE_B4, NOTE_G4, NOTE_A4, NOTE_E4, NOTE_FS4, 0, NOTE_B3, NOTE_D4, NOTE_FS4, NOTE_G4, NOTE_G4, 0, NOTE_FS4, NOTE_D4, NOTE_E4
+  0, 0, NOTE_B3, NOTE_D4, NOTE_E4, 0, NOTE_G4, NOTE_A4, NOTE_B4, NOTE_A4,
+  NOTE_G4, NOTE_FS4, NOTE_E4, 0, NOTE_B3, NOTE_E4, NOTE_FS4, NOTE_FS4, 0, NOTE_G4,
+  NOTE_A4, NOTE_G4, NOTE_FS4, NOTE_E4, 0, NOTE_B3, NOTE_B3, NOTE_D4, NOTE_E4, NOTE_E4,
+  NOTE_G4, NOTE_A4, NOTE_G4, NOTE_FS4, NOTE_D4, NOTE_E4, 0, NOTE_B3, NOTE_E4, NOTE_FS4,
+  NOTE_FS4, NOTE_FS4, 0, NOTE_G4, NOTE_A4, NOTE_G4, NOTE_FS4, NOTE_E4, NOTE_FS4, 0,
+  NOTE_G4, NOTE_A4, NOTE_FS4, NOTE_B4, NOTE_G4, NOTE_A4, NOTE_E4, NOTE_FS4, 0, NOTE_B3,
+  NOTE_D4, NOTE_FS4, NOTE_G4, NOTE_G4, 0, NOTE_FS4, NOTE_D4, NOTE_E4
 };
 
 double noteDurations[] = {
-  4, 4, 4, 4, 2, 4, 4, 4, 8, 8, 4, 4, 1, 4, 4, 4, 4, 1, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 2, 2, 4, 8, 8, 4, 4, 4, 2, 4, 4, 4, 4, 4, 1, 4, 8, 8, 4, 4, 4, 1.5, 4, 4, 2, 4, 1, 4, 4, 4, 1, 4, 4, 4, 4, 2, 2, 8, 2, 8, 0.5
+  4, 4, 4, 4, 2, 4, 4, 4, 8, 8,
+  4, 4, 1, 4, 4, 4, 4, 0.875, 4, 4,
+  4, 8, 8, 2, 4, 4, 4, 4, 2, 1.5,
+  8, 8, 4, 4, 4, 2, 4, 4, 4, 4,
+  4, 1, 4, 8, 8, 4, 4, 4, 1.5, 4,
+  4, 2, 4, 1, 4, 4, 4, 1, 4, 4,
+  4, 4, 2, 2, 8, 2, 8, 0.5
 };
 
 void setup() {
@@ -40,39 +52,45 @@ void setup() {
   lcd.print("mach mich munter");
 
   lcd.setCursor(8, 0);
+  riddleShake();
 }
 
-void loop() {
+void pressFriend() {
+  // while (true) {
+  //   button.loop();
 
-  button.loop();
-  if (button.isPressed()) {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Show me:");
-    lcd.setCursor(0, 1);
-    lcd.print("'Freunde'");
-    playVocabel();
-  }
-
-  //Kippschalter ab hier
-  switchStateKipp = digitalRead(switchPinKipp);
-
-  if (switchStateKipp != prevSwitchStateKipp) {
-    if (switchStateKipp == LOW) {
+  //   if (button.isPressed()) {
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("Sehr gut,");
+      lcd.print("Show me:");
       lcd.setCursor(0, 1);
-      lcd.print("kleiner Hobbit!");
-      playHobbit();
-    }
-  }
+      lcd.print("'Freunde'");
+      playVocabel();
+  //   }
+  // }
+}
 
-  prevSwitchStateKipp = switchStateKipp;
+void riddleShake() {
+  //Kippschalter ab hier
+
+  while (true) {
+    switchStateKipp = digitalRead(switchPinKipp);
+    if (switchStateKipp != prevSwitchStateKipp) {
+      if (switchStateKipp == LOW) {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Sehr gut,");
+        lcd.setCursor(0, 1);
+        lcd.print("kleiner Hobbit!");
+        playHobbit();
+      }
+    }
+    prevSwitchStateKipp = switchStateKipp;
+  }
 }
 
 void playHobbit() {
-  for (int thisNote = 0; thisNote < 64; thisNote++) {
+  for (int thisNote = 0; thisNote < 68; thisNote++) {
 
     // to calculate the note duration, take one second divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
@@ -92,6 +110,7 @@ void playHobbit() {
 
     noTone(8);
   }
+  pressFriend();
 }
 
 void playVocabel() {
@@ -100,13 +119,11 @@ void playVocabel() {
   while (loopMethod) {
     int analogReading = analogRead(FORCE_SENSOR_PIN);
 
-    Serial.println(analogReading);
-
-    if (analogReading > 900) {      
+    if (analogReading > 900) {
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Good job");
       loopMethod = false;
-    }    
+    }
   }
 }
